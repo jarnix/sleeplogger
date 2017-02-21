9acbd1790cc6c66b8babf0e821df3084c1fc12c7
Julien Ricard <jarnix@jarnix.com>
2017-02-21 22:47:32 +0100

    yo

---
+++ app/src/main/java/com/julien/sleeplogger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.julien.sleeplogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.database.sqlite.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
